003e552c98161a292e2343b796971b2d201a9a48
22npatrick <22npatrick@wakatipu.school.nz>
2025-08-25 22:04:39 +1200

    Website now not case senstive and not space senstive, changed some of website's styling

--- Script_CG.js
+++ Script_CG.js
@@ -5,43 +5,88 @@ let num = 0;
 dissolution of countries.
 There are currently 197 counties in the country array
 */
-const numberOfCountries = 10;
+const numberOfCountries = 197;
 const countryArray = [
-"Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
-"Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia",
-"Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada",
-"Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Costa Rica", "Croatia", "Cuba", "Cyprus",
-"Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador",
-"Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Federated States of Micronesia", "Fiji",
-"Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau",
-"Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast",
-"Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon",
-"Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali",
-"Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco",
-"Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea",
-"North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-"Philippines", "Poland", "Portugal", "Qatar", "Republic of the Congo", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
-"Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal",
-"Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
-"South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan",
-"Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine",
-"United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela",
-"Vietnam", "Yemen", "Zambia", "Zimbabwe"];
+"afghanistan", "albania", "algeria", "andorra", "angola", "antiguaandbarbuda", "argentina", "armenia", "australia", "austria",
+"azerbaijan", "bahamas", "bahrain", "bangladesh", "barbados", "belarus", "belgium", "belize", "benin", "bhutan", "bolivia",
+"bosniaandherzegovina", "botswana", "brazil", "brunei", "bulgaria", "burkinafaso", "burundi", "cambodia", "cameroon", "canada",
+"capeverde", "centralafricanrepublic", "chad", "chile", "china", "colombia", "comoros", "costarica", "croatia", "cuba", "cyprus",
+"czechrepublic", "democraticrepublicofthecongo", "denmark", "djibouti", "dominica", "dominicanrepublic", "timor-leste", "ecuador",
+"egypt", "elsalvador", "equatorialguinea", "eritrea", "estonia", "eswatini", "ethiopia", "federatedstatesofmicronesia", "fiji",
+"finland", "france", "gabon", "gambia", "georgia", "germany", "ghana", "greece", "grenada", "guatemala", "guinea", "guinea-bissau",
+"guyana", "haiti", "honduras", "hungary", "iceland", "india", "indonesia", "iran", "iraq", "ireland", "israel", "italy", "ivorycoast",
+"jamaica", "japan", "jordan", "kazakhstan", "kenya", "kiribati", "kosovo", "kuwait", "kyrgyzstan", "laos", "latvia", "lebanon",
+"lesotho", "liberia", "libya", "liechtenstein", "lithuania", "luxembourg", "madagascar", "malawi", "malaysia", "maldives", "mali",
+"malta", "marshallislands", "mauritania", "mauritius", "mexico", "moldova", "monaco", "mongolia", "montenegro", "morocco",
+"mozambique", "myanmar", "namibia", "nauru", "nepal", "netherlands", "newzealand", "nicaragua", "niger", "nigeria", "northkorea",
+"northmacedonia", "norway", "oman", "pakistan", "palau", "palestine", "panama", "papuanewguinea", "paraguay", "peru",
+"philippines", "poland", "portugal", "qatar", "republicofthecongo", "romania", "russia", "rwanda", "saintkittsandnevis",
+"saintlucia", "saintvincentandthegrenadines", "samoa", "sanmarino", "saotomeandprincipe", "saudiarabia", "senegal",
+"serbia", "seychelles", "sierraleone", "singapore", "slovakia", "slovenia", "solomonislands", "somalia", "southafrica",
+"southkorea", "southsudan", "spain", "srilanka", "sudan", "suriname", "sweden", "switzerland", "syria", "taiwan", "tajikistan",
+"tanzania", "thailand", "togo", "tonga", "trinidadandtobago", "tunisia", "turkey", "turkmenistan", "tuvalu", "uganda", "ukraine",
+"unitedarabemirates", "unitedkingdom", "unitedstates", "uruguay", "uzbekistan", "vanuatu", "vaticancity", "venezuela",
+"vietnam", "yemen", "zambia", "zimbabwe"];
 const Evens = [2,4,6,8,10];
-
-/*
-for (let i in countryArray) {
-    countryArray[i] = countryArray[i].toLowerCase()
-    console.log(countryArray[i])
-}
-*/
 const countriesGuessCorrectly = []
-var pop = "";
 
 /*
 const guessInput = document.getElementById("countryInput");
 const guessBtn =  document.getElementById("InputButton");
 */
+
+function myFunction(value) {
+    return value;
+}
+
+function giveup(){
+    console.log("giveup")
+    message.innerHTML = "";
+    console.log(countriesGuessCorrectly)
+    const Oceania = ["samoa", "australia"];
+    const Europe = ["latvia", "denmark"];
+    const Asia = ["china", "india"];
+    const Africa = ["south africa", "egypt"];
+    const NorthAmerica = ["united states", "canada"];
+    const SouthameriaAmerica = ["brazil", "argentina"];
+    let fullmessage = ""    
+    let checker = (arr, target) => target.every(v => arr.includes(v));  //Credit to https://stackoverflow.com/questions/53606337/check-if-array-contains-all-elements-of-another-array
+    if (checker(countriesGuessCorrectly, Oceania)){
+        fullmessage += " You guess all the countries in Oceania. "
+    } if (checker(countriesGuessCorrectly, Europe)){
+        fullmessage += " You guess all the countries in Europe, "
+    }  if (checker(countriesGuessCorrectly, Asia)){
+        fullmessage += " You guess all the countries in Asia, "
+    }  if (checker(countriesGuessCorrectly, Africa)){
+        fullmessage += " You guess all the countries in Africa, "
+    }  if (checker(countriesGuessCorrectly, NorthAmerica)){
+        fullmessage += " You guess all the countries in North America, "
+    }  if (checker(countriesGuessCorrectly, SouthameriaAmerica)){
+        fullmessage += " You guess all the countries in South America, "
+    }
+    var dave = document.getElementById("allSvg")
+    dave.remove();
+    dave = document.getElementById("countryInput")
+    dave.remove();
+    dave = document.getElementById("InputButton") 
+    dave.remove();
+    dave = document.getElementById("giveUpButton")
+    dave.remove();
+    var countryElement = document.createElement("p");
+    countryElement.innerHTML = fullmessage;
+    document.getElementById("List").appendChild(countryElement);
+
+/*
+    if (num <= (numberOfCountries*0.05)){
+        message.innerHTML = "... You need to learn your countries";
+    } else if (num <= (numberOfCountries*0.10)){
+        message.innerHTML = "You know over 10% of the countries in the world! Keep own learning your countries ";
+    }
+*/  
+    return;
+}
+
+
 // Function to 
 function guess(){
     // Check if you have already guessed all the countries correctly
@@ -49,8 +94,9 @@ function guess(){
          document.getElementById("percentage").innerHTML = "You are done";
     }
     else{
-        var userGuess = document.getElementById("countryInput").value;
-        // userGuess = userGuess.toLowerCase()
+        var baseGuess = document.getElementById("countryInput").value;
+        userGuess = baseGuess.toLowerCase()
+        userGuess = userGuess.replace(/\s/g, "");
         // Checks if the country is in the countryArray 
         if (countryArray.includes(userGuess)) {
             var countryElement = document.createElement("p");
@@ -64,21 +110,14 @@ function guess(){
             countryElement.innerHTML = userGuess + num;
             console.log(num)
             num++;
-            
             countryInput.value = ""; //Clear the input field after a guess
             document.getElementById("percentage").innerHTML = num + "/" + String(numberOfCountries);
             //document.getElementById("List").appendChild(countryElement);
             message.innerHTML = "";
-            pop = userGuess
-            x = pop
-            console.log(pop)
-            console.log(x)
-            x = x.replace(/\s/g, "");
-            console.log(x)
-            x = "#" + x 
-            console.log(x)
-            document.querySelectorAll(x).forEach(e=>{
+            userGuess = "#" + userGuess         
+            document.querySelectorAll(userGuess).forEach(e=>{
                 e.style.fill="black";
+                e.style.border="white";
                 }
             )
         } else if (countriesGuessCorrectly.includes(userGuess)) {
@@ -93,6 +132,3 @@ function guess(){
         }
     }
 }
-
-
-
